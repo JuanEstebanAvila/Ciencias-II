795229fc806f5f1e1ad2a033dbe5609e82cf9a00
SpeedyR01 <soriveraf@udistrital.edu.co>
2026-06-26 14:21:54 -0500

    Delete Incidencia_Adyacencia/Grafo.java

--- Incidencia_Adyacencia/Grafo.java
+++
@@ -1,9 +0,0 @@
-package Incidencia_Adyacencia;
-
-import java.util.List;
-
-public interface Grafo {
-    void agregarArista(int origen, int destino, int peso, boolean dirigido);
-
-    List<Arista> obtenerAdyacentes(int nodo);
-}
